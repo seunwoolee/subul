830a1f7061eb3e981e90ebd04d54f740bd0e6b2a
seungwoolee <sugohaseyo93@gmail.com>
2019-01-25 14:35:42 +0900

    공인 ip 추가 수정

--- core/0-2_productUnitPriceShell.py
+++ core/0-2_productUnitPriceShell.py
@@ -11,7 +11,7 @@ START_VALUE = u"Unicode \u3042 3".encode('utf-8')
 END_VALUE = u"Unicode \u3042 6".encode('utf-8')
 # cx_Oracle 한글처리 끝
 
-con = cx_Oracle.connect('system/kcerp@155.1.19.2/kcerp')
+con = cx_Oracle.connect('system/kcerp@112.216.66.219/kcerp')
 cursor = con.cursor()
 query = " select 거래처 as location , 제품 as product, 금액 as price , 사용유무 as delete_state from  KCFEED.FRESH단가"
 cursor.execute(query)
