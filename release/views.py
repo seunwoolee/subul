609b90ddd31654c98cbb54fff019e05f763992cf
seungwoolee <sugohaseyo93@gmail.com>
2019-03-08 11:23:23 +0900

    거래명세표 pdf 수정

--- release/views.py
+++ release/views.py
@@ -47,11 +47,11 @@ class GeneratePDF(View):
         pdf = render_to_pdf('invoice/출고거래명세표.html', context_dict)
         if pdf:
             response = HttpResponse(pdf, content_type='application/pdf')
-            filename = "거래명세표_{}_{}.pdf".format(yyyymmdd, location.codeName)
-            content = "inline; filename={}".format(filename).encode('utf-8')
+            filename = "invoice.pdf"
+            content = "inline; filename=%s" % (filename)
             download = request.GET.get("download")
             if download:
-                content = "attachment; filename={}".format(filename)
+                content = "attachment; filename=%s" % (filename)
             response['Content-Disposition'] = content
             return response
         return HttpResponse("Not found")
